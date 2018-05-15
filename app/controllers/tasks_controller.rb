6aef42dd48b7bebd197c059596ff90707cef86c0
AutumnIDavis <foreverautumn16@outlook.com>
2018-05-15 15:21:56 -0400

    Added ability to delete without reloading the page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,6 +22,9 @@ class TasksController < ApplicationController
   # GET /tasks/new
   def new
     @task = Task.new
+    respond_to do |format|
+      format.js
+    end
   end
 
   # GET /tasks/1/edit
@@ -37,6 +40,7 @@ class TasksController < ApplicationController
       if @task.save
         format.html { redirect_to @task, notice: 'Task was successfully created.' }
         format.json { render :show, status: :created, location: @task }
+        format.js
       else
         format.html { render :new }
         format.json { render json: @task.errors, status: :unprocessable_entity }
@@ -63,8 +67,9 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
-      format.json { head :no_content }
+
+    #   format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
+      format.js
     end
   end
 
